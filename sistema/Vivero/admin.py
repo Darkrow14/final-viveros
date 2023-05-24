a9417de8f0726d821126deb8eb51c270a51cbf49
Yeimy Juliana Guarin <yjguarin@utp.edu.co>
2023-05-24 01:01:28 -0500

    Se crearon las vistas iniciales para los modulos

--- sistema/Vivero/admin.py
+++ sistema/Vivero/admin.py
@@ -1,6 +1,3 @@
 from django.contrib import admin
-from .models import *
 
-admin.site.register(Departamento)
-admin.site.register(Municipio)
-admin.site.register(Vivero)
+# Register your models here.
