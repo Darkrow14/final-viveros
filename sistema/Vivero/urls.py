5958f93011f0d1bbdef517d2d737fead0e0042fb
jhonf <jhonfuto19@gmail.com>
2023-05-25 01:10:56 -0500

    Se realizan ajustes visuales y se da la opción de exportar los productores y viveros. Se añaden iconos

--- sistema/Vivero/urls.py
+++ sistema/Vivero/urls.py
@@ -11,6 +11,8 @@ urlpatterns = [
     path('listarVivero/',login_required(ListarVivero.as_view()), name="listar_vivero"),
     path('editarVivero/<int:pk>/', login_required(EditarVivero.as_view()), name='editar_vivero'),
     path('eliminarVivero/<int:pk>/', login_required(EliminarVivero.as_view()), name='eliminar_vivero'),
+    path('descargarexcel', login_required(views.exportar_viveros_excel), name ="exportarexcel"),
+    path('descargarpdf', login_required(views.exportar_viveros_pdf), name ="exportarpdf"),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
